abdef54bab409a587c4ae6d0d3e48e353da5dd80
jkapkor@gmail.com <jkapkor@gmail.com>
2025-05-13 19:35:36 +0300

    practising with numpy library

---
+++ learn_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+#crate a simple array
+my_array = np.array([1, 2, 3, 4, 5])
+# print the array
+print("My array is: ", my_array)
+# Perform operations
+print("Array * 2:", my_array * 2)         # Multiply each element by 2
+print("Mean:", np.mean(my_array))        # Average of the array
+print("Square Roots:", np.sqrt(my_array))# Square root of each element
+print("Sum:", np.sum(my_array))          # Sum of all elements
